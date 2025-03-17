3645e911534ea3f71a9d7e9b28cef6fb5c102ebe
micfoster <miclfoster420@gmail.com>
2025-03-17 13:23:49 -0400

    validator for get point

--- src/routes/receipts.route.ts
+++ src/routes/receipts.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getPointsById, processReceipt } from "../controllers/receipt.controller";
 import { validateReceiptMiddleware } from "../middleware/validateReceipt";
+import { validateReceiptIdMiddleware } from "../middleware/validateReceiptId";
 
 const receiptRouter = Router();
 
 receiptRouter.post('/process', validateReceiptMiddleware, processReceipt);
-receiptRouter.get('/:id/points', getPointsById);
+receiptRouter.get('/:id/points', validateReceiptIdMiddleware, getPointsById);
 
 export default receiptRouter;
